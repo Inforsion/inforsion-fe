b53ba5aeba9d407cc694fea9c7449be54d4ee976
yungan9 <geyun105@naver.com>
2024-07-19 02:17:47 +0900

    Design:GlobalStyle Setting

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import GlobalStyle from './style/GlobalStyle.js'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  max-width: 430px;
+    height: 100vh;
+    position: relative;
+    margin: 0 auto;
+    background-color: #ffffff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    overflow: auto;
+    justify-content: center;
+`
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle/>
+      <Container>
+        <App/>
+      </Container>
   </React.StrictMode>,
 )
---
+++ src/style/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    colors: {
+      primary: '#0070f3',
+      background: '#F6F9F0',
+    },
+    fonts: {
+      main: 'Inter, sans-serif',
+    },
+  };
+  
