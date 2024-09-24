729d8a3167173bd6cae049f61f9e9bf1d5992bd3
yungan9 <geyun105@naver.com>
2024-09-24 17:32:16 +0900

    Feat:Implement MenuList Component

---
+++ src/components/List/MenuList.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+
+const ListContainer = styled.div`
+    width: 95%;
+    height: 80px;
+    border-Bottom: 1px solid #D3D3D3;
+    margin: 10px auto;
+    padding: 4px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const Image = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #D0D0D0;
+    display: flex;
+    border-radius: 8px;
+
+`
+
+const Num = styled.div`
+    display: flex;
+    font-weight: 400;
+    padding: 5px;
+    font-size: 16px;
+    color: #272525;
+`
+
+const Content = styled.div`
+    display: flex;
+    padding-left: 5px;
+    margin-top: 2px;
+    font-size: 16px;
+    color: #272525;
+
+`
+
+
+function MenuList(){
+    return(
+        <>
+        <ListContainer>
+            <Num>01</Num>
+            <Image/>
+            <Content>아메리카노</Content>
+            <Content>2800</Content>
+        </ListContainer>
+        </>
+    );
+}
+
+export default MenuList;
--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,111 +1,124 @@
-import React from 'react'
-import styled from 'styled-components';
-import MainHeader from '../components/MainHeader';
-import InputButton from '../components/InputButton';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import MainHeader from "../components/MainHeader";
+import InputButton from "../components/InputButton";
+import MenuList from "../components/List/MenuList";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    flex-direction: column;
-
-`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+`;
 const StyledLink = styled(Link)`
-    text-decoration: none;
-    align-self: flex-end;
-    margin-right: 7%;
-
+  text-decoration: none;
+  align-self: flex-end;
+  margin-right: 7%;
 `;
 
 const MenuBtn = styled.button`
-    margin-top: 20px;
-    width: 124px;
-    height: 28px;
-    border-radius: 10px;
-    font-size: 12px;
-    font-weight: 600;
-    color: #ffffff;
-    background-color: #2897FF;
-`
+  margin-top: 20px;
+  width: 124px;
+  height: 28px;
+  border-radius: 10px;
+  font-size: 12px;
+  font-weight: 600;
+  color: #ffffff;
+  background-color: #2897ff;
+`;
 
 const Box_One = styled.div`
-    display: flex;
-    align-content: center;
-    flex-direction: column;
-    width: 86%;
-    height: 300px;
-    border: 1px solid #BDBDBD;
-    border-radius: 9px;
-    margin: auto;
-    margin-top: 30px;
-`
+  display: flex;
+  align-content: center;
+  flex-direction: column;
+  width: 86%;
+  height: 300px;
+  border: 1px solid #bdbdbd;
+  border-radius: 9px;
+  margin: auto;
+  margin-top: 30px;
+`;
 
 const Image = styled.div`
-    width: 100px;
-    height: 100px;
-    border-radius: 100%;
-    background-color: #F2F2F2;
-    margin-left: 36%;
-    margin-top: 20px;
-
-`
+  width: 100px;
+  height: 100px;
+  border-radius: 100%;
+  background-color: #f2f2f2;
+  margin-left: 36%;
+  margin-top: 20px;
+`;
 const InputContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    margin-top: 30px;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 30px;
+`;
 
 const Context = styled.div`
-    font-size: 12px;
-    font-weight: 700;
-    margin-left: 20px;
-`
+  font-size: 12px;
+  font-weight: 700;
+  margin-left: 20px;
+`;
 
 const Input = styled.input`
-    width: 150px;
-    height: 15px;
-    font-size: 12px;
-    border: none;
-    margin-right: 20%;
-`
+  width: 150px;
+  height: 15px;
+  font-size: 12px;
+  border: none;
+  margin-right: 20%;
+`;
 
 const IngredientBtn = styled.button`
-    width: 65px;
-    height: 16px;
-    margin-right: 44%;
-    border-radius: 5px;
-    font-size: 8px;
-    font-weight: 400;
-    color: #ffffff;
-    background-color: #2897FF;
-`
-
+  width: 65px;
+  height: 16px;
+  margin-right: 44%;
+  border-radius: 5px;
+  font-size: 8px;
+  font-weight: 400;
+  color: #ffffff;
+  background-color: #2897ff;
+`;
+const Box_two = styled.div`
+  width: 86%;
+  height: 560px;
+  border: 1px solid #bdbdbd;
+  border-radius: 9px;
+  margin: auto;
+  margin-top: 30px;
+`;
 
 export default function MenuPage() {
   return (
     <>
-    <MainHeader/>
-    <Container>
-        <StyledLink to ="/main"><MenuBtn>인포전 홈 {">"}</MenuBtn></StyledLink>
+      <MainHeader />
+      <Container>
+        <StyledLink to="/main">
+          <MenuBtn>인포전 홈 {">"}</MenuBtn>
+        </StyledLink>
         <Box_One>
-            <Image/>
-            <InputContainer>
-                <Context>상품명</Context>
-                <Input placeholder='상품이름을 입력해주세요'/>
-            </InputContainer>
-            <InputContainer>
-                <Context>판매 가격</Context>
-                <Input placeholder='가격을 입력해주세요'/>
-            </InputContainer>
-            <InputContainer>
-                <Context>1개당 재고용량</Context>
-                <IngredientBtn>재료 추가하기</IngredientBtn>
-            </InputContainer>
+          <Image />
+          <InputContainer>
+            <Context>상품명</Context>
+            <Input placeholder="상품이름을 입력해주세요" />
+          </InputContainer>
+          <InputContainer>
+            <Context>판매 가격</Context>
+            <Input placeholder="가격을 입력해주세요" />
+          </InputContainer>
+          <InputContainer>
+            <Context>1개당 재고용량</Context>
+            <IngredientBtn>재료 추가하기</IngredientBtn>
+          </InputContainer>
         </Box_One>
-      <InputButton text="저장"/>
-    </Container>
+        <InputButton text="저장" />
+        <Box_two>
+          <MenuList />
+          <MenuList />
+          <MenuList />
+          <MenuList />
+        </Box_two>
+      </Container>
     </>
-  )
+  );
 }
