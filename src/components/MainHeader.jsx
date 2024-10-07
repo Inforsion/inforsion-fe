53d9086fd5e36429c9aca6f41bc91f4a89002bdb
yungan9 <geyun105@naver.com>
2024-10-08 01:10:28 +0900

    Fix:Resolve issue with Sidebar not fully closing

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -5,68 +5,61 @@ import profile from "../assets/image/Profile.svg?react";
 import Sidebar from "./Sidebar";
 
 const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: row;
-    justify-content: space-between;
-    height: 60px;
-    padding: 20px;
-    z-index: 2;
-    position: relative;
-    `
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 60px;
+  padding: 20px;
+  z-index: 2;
+  position: relative;
+`;
 
 const LogoText = styled.div`
-    padding-left:30px;
-    font-size: 20px;
-    font-weight: 600;
-    color: #343C6A;
-`
+  padding-left: 30px;
+  font-size: 20px;
+  font-weight: 600;
+  color: #343c6a;
+`;
 
 const IconWrapper = styled.div`
-        cursor: pointer;
-
-`
-
+  cursor: pointer;
+`;
 
 const SidebarWrapper = styled.div`
-    position: absolute;
-    left: ${(props) => (props.isOpen ? '0' : '-300px')};
-    top: 0;
-    height: 80%;
-    width: 400px;
-    transition: left 0.3s ease;
-    z-index: 1;
-`
-
+  position: absolute;
+  left: ${(props) => (props.isOpen ? "0" : "-400px")};
+  top: 0;
+  height: 80%;
+  width: 400px;
+  transition: left 0.3s ease;
+  z-index: 1;
+`;
 
 function Icons({ icon: Icon, width, height }) {
-    return (
-        <Icon width={width} height={height} fill="none" />
-    );
+  return <Icon width={width} height={height} fill="none" />;
 }
 
 function MainHeader() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
-    return (
+  return (
     <>
-        <Container>
-            <IconWrapper onClick={toggleSidebar}>
-                <Icons icon={tab} width={15} height={18}/>
-            </IconWrapper>        
-            <LogoText>Inforsion</LogoText>
-            <Icons icon={profile} width={40} height={40} />
-        </Container>
-        <SidebarWrapper isOpen={isSidebarOpen}>
-                <Sidebar />
-        </SidebarWrapper>
-
+      <Container>
+        <IconWrapper onClick={toggleSidebar}>
+          <Icons icon={tab} width={15} height={18} />
+        </IconWrapper>
+        <LogoText>Inforsion</LogoText>
+        <Icons icon={profile} width={40} height={40} />
+      </Container>
+      <SidebarWrapper isOpen={isSidebarOpen}>
+        <Sidebar />
+      </SidebarWrapper>
     </>
   );
 }
