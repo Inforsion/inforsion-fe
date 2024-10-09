d477c98dacf2985d17cbc591ccd2d1eaaf8490d5
yungan9 <geyun105@naver.com>
2024-10-09 17:25:51 +0900

    Style:Adjust button spacing

--- src/pages/SalesPage.jsx
+++ src/pages/SalesPage.jsx
@@ -1,13 +1,12 @@
-import {React, useState} from 'react';
-import styled from 'styled-components';
-import MainHeader from '../components/MainHeader';
-import Navigation from '../components/Navigation';
-import MainSalesChart from '../components/Main/MainSalesChart';
+import { React, useState } from "react";
+import styled from "styled-components";
+import MainHeader from "../components/MainHeader";
+import Navigation from "../components/Navigation";
+import MainSalesChart from "../components/Main/MainSalesChart";
 import Sales from "../assets/image/sales.svg?react";
-import ChartButton from '../components/Main/ChartButton';
-import { Link } from 'react-router-dom';
+import ChartButton from "../components/Main/ChartButton";
+import { Link } from "react-router-dom";
 import { salesData } from "../SalesData";
-
 
 const Container = styled.div`
   display: flex;
@@ -36,26 +35,26 @@ const ValueContainer1 = styled.div`
   display: flex;
   flex-direction: column;
   margin: 12px;
-`
+`;
 
 const SalesText = styled.div`
   font-size: 12px;
   font-weight: 400;
   margin-bottom: 10px;
   color: #343434;
-`
+`;
 
 const ValueContainer = styled.div`
   display: flex;
   flex-direction: row;
-`
+`;
 
 const SalesValue = styled.div`
   display: flex;
   font-size: 28px;
   font-weight: 500;
-  color: ${(props) => (props.isrefund ? '#FF7173' : 'black')};
-`
+  color: ${(props) => (props.isrefund ? "#FF7173" : "black")};
+`;
 
 const ValueText = styled.div`
   display: flex;
@@ -63,27 +62,26 @@ const ValueText = styled.div`
   font-size: 15px;
   font-weight: 400;
   color: #343434;
-`
+`;
 
 const BtnContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  margin: 20px;
   padding-top: 20px;
-`
+  gap: 4px;
+`;
 
 const Btn = styled.button`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  width: 180px;
   height: 88px;
   border-radius: 9px;
   font-size: 8px;
   font-weight: 400;
   color: #ffffff;
   background-color: #2897ff;
-  padding: 10px;
+  padding: 16px;
 `;
 
 const BtnContext = styled.p`
@@ -91,89 +89,87 @@ const BtnContext = styled.p`
   font-weight: 400;
   color: #ffffff;
   margin-bottom: 4px;
-`
+`;
 const BtnP = styled.p`
   text-align: left;
   font-size: 12px;
   font-weight: 300;
   color: #ffffff;
-`
+`;
 
 const SalesHeader = styled.div`
-  display: flex ;
+  display: flex;
   flex-direction: row;
   margin-top: 30px;
   padding-bottom: 30px;
   margin-left: 20px;
-  ;
-`
+`;
 
 const ChartText = styled.div`
   font-size: 15px;
   font-weight: 600;
   margin: 5px;
-`
+`;
 
 export default function SalesPage() {
   const [activeTab, setActiveTab] = useState("day");
 
   return (
-    <>      
-    <MainHeader />
-    <Container>
-      <Box>
-        <ValueContainer1>
-          <SalesText>현 매출</SalesText>
-          <ValueContainer>
-            <SalesValue>289,980</SalesValue>
-            <ValueText>원</ValueText>
-          </ValueContainer>
-        </ValueContainer1>
-        <div style={{padding: 32}}></div>        
-        <ValueContainer1>
-          <SalesText>순이익</SalesText>
-          <ValueContainer>
-            <SalesValue>115,890</SalesValue>
-            <ValueText>원</ValueText>
-          </ValueContainer>
-        </ValueContainer1>
-        <ValueContainer1>
-          <SalesText>환불금액</SalesText>
-          <ValueContainer>
-            <SalesValue isrefund={true}>-23,400</SalesValue>
-            <ValueText>원</ValueText>
-          </ValueContainer>
-        </ValueContainer1>
-      </Box>
+    <>
+      <MainHeader />
+      <Container>
+        <Box>
+          <ValueContainer1>
+            <SalesText>현 매출</SalesText>
+            <ValueContainer>
+              <SalesValue>289,980</SalesValue>
+              <ValueText>원</ValueText>
+            </ValueContainer>
+          </ValueContainer1>
+          <div style={{ padding: 32 }}></div>
+          <ValueContainer1>
+            <SalesText>순이익</SalesText>
+            <ValueContainer>
+              <SalesValue>115,890</SalesValue>
+              <ValueText>원</ValueText>
+            </ValueContainer>
+          </ValueContainer1>
+          <ValueContainer1>
+            <SalesText>환불금액</SalesText>
+            <ValueContainer>
+              <SalesValue isrefund={true}>-23,400</SalesValue>
+              <ValueText>원</ValueText>
+            </ValueContainer>
+          </ValueContainer1>
+        </Box>
 
-      <BtnContainer>
-        <Link to ="/writesales">
+        <BtnContainer>
+          <Link to="/writesales">
+            <Btn>
+              <BtnContext>매출 작성하러 가기 {">"}</BtnContext>
+              <BtnP>
+                오늘의 매출을 어디서든 기록하고 <br />
+                수정할 수 있습니다.
+              </BtnP>
+            </Btn>
+          </Link>
           <Btn>
-            <BtnContext>매출 작성하러 가기 {">"}</BtnContext>
+            <BtnContext>영수증 촬영하러 가기 {">"}</BtnContext>
             <BtnP>
-              오늘의 매출을 어디서든 기록하고 <br />
-              수정할 수 있습니다.
+              영수증 촬영으로 간편한 매출/재고 <br />
+              관리를 할 수 있습니다.
             </BtnP>
           </Btn>
-        </Link>
-        <Btn>
-          <BtnContext>영수증 촬영하러 가기 {">"}</BtnContext>
-          <BtnP>
-            영수증 촬영으로 간편한 매출/재고 <br />
-            관리를 할 수 있습니다.
-          </BtnP>
-        </Btn>
-      </BtnContainer>
+        </BtnContainer>
 
-      <SalesHeader>
-        <Sales />
-        <ChartText>매출 그래프</ChartText>
-      </SalesHeader>
-      <ChartButton activeTab={activeTab} setActiveTab={setActiveTab} />
-          <MainSalesChart data={salesData[activeTab]} />
+        <SalesHeader>
+          <Sales />
+          <ChartText>매출 그래프</ChartText>
+        </SalesHeader>
+        <ChartButton activeTab={activeTab} setActiveTab={setActiveTab} />
+        <MainSalesChart data={salesData[activeTab]} />
+      </Container>
       <Navigation />
-    </Container>
     </>
-
   );
 }
