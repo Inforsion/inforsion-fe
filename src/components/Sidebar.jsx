3b264c982a458f83f5c3e3eb7373721516cde760
yungan9 <geyun105@naver.com>
2024-10-09 17:29:30 +0900

    Feat:Connect Sidebar to Pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import SidebarList from './Main/SidebarList';
+import React from "react";
+import styled from "styled-components";
+import SidebarList from "./Main/SidebarList";
 import Sales from "../assets/image/IconB/salesB.svg?react";
 import Goods from "../assets/image/IconB/goodsB.svg?react";
 import Operate from "../assets/image/IconB/operateB.svg?react";
@@ -8,11 +8,12 @@ import Menu from "../assets/image/IconB/menuB.svg?react";
 import Logout from "../assets/image/IconB/logoutB.svg?react";
 import SearchIcon from "../assets/image/search.svg?react";
 import InputButton from "./InputButton";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   background-color: white;
   width: 60%;
-  box-shadow: 5px 5px 5px rgba(0,0,0,0.2);
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
   margin-top: 60px;
   overflow: auto;
 `;
@@ -28,7 +29,7 @@ const Search = styled.input`
   height: 52px;
   border-radius: 16px;
   border: none;
-  background-color: #F5F5F5;
+  background-color: #f5f5f5;
   padding-left: 50px;
   outline: none;
 `;
@@ -48,14 +49,22 @@ export default function Sidebar() {
         <Icon>
           <SearchIcon />
         </Icon>
-        <Search placeholder='Search...' />
+        <Search placeholder="Search..." />
       </SearchWrapper>
-      <SidebarList icon={Sales} text="매출" />
-      <SidebarList icon={Goods} text="재고" />
-      <SidebarList icon={Operate} text="운영" />
+      <Link to="/sales">
+        <SidebarList icon={Sales} text="매출" />
+      </Link>
+      <Link to="/goods">
+        <SidebarList icon={Goods} text="재고" />
+      </Link>
+      <Link to="/operate">
+        <SidebarList icon={Operate} text="운영" />
+      </Link>
       <SidebarList icon={Menu} text="우리 가게 메뉴" />
-      <div style={{ margin: '20%' }}></div>
-      <InputButton text="메뉴등록하러가기 >"/>
+      <div style={{ margin: "20%" }}></div>
+      <Link to="/menu">
+        <InputButton text="메뉴등록하러가기 >" />{" "}
+      </Link>
       <SidebarList icon={Logout} text="Logout" />
     </Container>
   );
